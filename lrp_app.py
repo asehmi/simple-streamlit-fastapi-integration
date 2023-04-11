db6fbecb5477ed1a9bf83fe9a2f97548d493371c
asehmi <vin@thesehmis.com>
2023-04-12 00:31:39 +0100

    HOST rename localhost >> 127.0.0.1

--- lrp_app.py
+++ lrp_app.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 
 # --------------------------------------------------------------------------------
 
-API_HOST='localhost'
+API_HOST='127.0.0.1'
 API_PORT=5000
 API_BASE_URL=f'http://{API_HOST}:{API_PORT}'
 
